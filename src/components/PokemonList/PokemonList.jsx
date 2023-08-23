746ade4d644688cc5d1f3ad324545535ffce64a1
HemantNavlani <hemant.navlani.0506@gmail.com>
2023-08-23 11:45:12 +0530

    Added pagination

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -10,21 +10,34 @@ import Pokemon from "../Pokemon/Pokemon";
     //     console.log("effect called")
     // },[x]) //x ka change track ho raha hai,kyunki voh dependency array me hai 
 
-
+   
 function PokemonList(){
+
 
     const [pokemonList,setPokemonList] = useState([])
     const [isLoading,setIsLoading] = useState(true);
 
-    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon' //pokemon api
+    const [pokedexUrl,setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon') //pokemon api
+
+
+    const [nextUrl,setNextUrl] = useState('')
+    const [prevUrl,setPrevUrl] = useState('')
 
     async function downloadPokemons(){
-        const response = await axios.get(POKEDEX_URL) // //this downloads list of 20 pokemons
+        setIsLoading(true);
+        
+        const response = await axios.get(pokedexUrl) // //this downloads list of 20 pokemons
 
 
         const pokemonResults = response.data.results; //we get the array of pokemons from result 
 
         console.log(response.data)
+
+        setNextUrl(response.data.next)
+        console.log(nextUrl)
+        setPrevUrl(response.data.previous)
+        console.log(prevUrl)
+
 
         //iterating over the array of pokemons, and using their url  to create an array of promises that will download those 20 pokemons
         const pokemonResultPromise =  pokemonResults.map((pokemon)=>axios.get(pokemon.url))
@@ -47,11 +60,13 @@ function PokemonList(){
 
         console.log(pokeListResult)
         setPokemonList(pokeListResult);
-        setIsLoading(false)
+        setIsLoading(false);
     }
-    useEffect(async ()=>{
+
+
+    useEffect( ()=>{
         downloadPokemons();
-    },[]) 
+    },[pokedexUrl]);
 
     
     return (
@@ -66,8 +81,11 @@ function PokemonList(){
             </div>
 
             <div className="controls">
-                <button>Prev</button>
-                <button>Next</button>
+                <button disabled={prevUrl==null} onClick={()=>setPokedexUrl(prevUrl)
+                }>Prev</button>
+                <button disabled={nextUrl==null} onClick={()=>{setPokedexUrl(nextUrl)
+                console.log(pokedexUrl)    }
+                }>Next</button>
             </div>
            
 
